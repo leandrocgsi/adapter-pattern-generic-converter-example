6bbfc1a480885ee1f0e939a18f846184573995db
leandrocgsi <leandrocgsi@gmail.com>
2016-11-17 18:07:10 -0200

    Starting code

---
+++ src/main/java/com/uolinc/commons/adapter/implementation/JsonConverter.java
@@ -0,0 +1,34 @@
+package com.uolinc.commons.adapter.implementation;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.uolinc.commons.adapter.interfaces.AdvancedSchemaConverter;
+
+public class JsonConverter<T> implements AdvancedSchemaConverter<T>{
+
+	private Class<T> clazz;
+
+	public JsonConverter(Class<T> clazz) {
+		super();
+		this.clazz = clazz;
+	}
+	
+	public T convertJson(String data) {
+		ObjectMapper mapper = new ObjectMapper();
+		T user = null;
+		try {
+			user = mapper.readValue(data, clazz);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+}
