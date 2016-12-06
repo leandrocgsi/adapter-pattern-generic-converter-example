53e611d6e14a835103b33ada7e502eb2a534904e
leandrocgsi <leandrocgsi@gmail.com>
2016-12-06 19:37:16 -0200

    Quick refactory

--- src/test/java/br/com/erudio/adapter/interfaces/RiskAnalysisConverterTest.java
+++ src/test/java/br/com/erudio/adapter/interfaces/RiskAnalysisConverterTest.java
@@ -17,39 +17,39 @@ import br.com.erudio.data.User;
 
 public class RiskAnalysisConverterTest {
 
-	@Test
-	public void testConverter() throws JsonGenerationException, JsonMappingException, IOException {
-		GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
-		String data = "{\"age\":31,\"messages\":[\"Message 1\",\"Message 2\",\"Message 3\",\"Message 4\"],\"name\":\"Leandro Costa\"}";
-		
-		User user = (User) schemaConverter.convert("json", data);
-	    Assert.assertEquals(user.getAge(), 31);
-	    Assert.assertEquals(user.getName(), "Leandro Costa");
-	    Assert.assertEquals(user.getMessages().size(), 4);
-	}
-	
-	@Test
-	public void testConverterXML() throws JsonGenerationException, JsonMappingException, IOException {
-		GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
-		String data = "<User xmlns=\"\"><name>Leandro Costa</name><age>31</age><messages><messages>Message 1</messages><messages>Message 2</messages><messages>Message 3</messages><messages>Message 4</messages></messages></User>";
-		
-		User user = (User) schemaConverter.convert("xml", data);
-	    Assert.assertEquals(user.getAge(), 31);
-	    Assert.assertEquals(user.getName(), "Leandro Costa");
-	    Assert.assertEquals(user.getMessages().size(), 4);
-	}
-	
-	@Test
-	public void testWrite() throws JsonGenerationException, JsonMappingException, IOException {
-		GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
-		String data = "{\"age\":31,\"messages\":[\"Message 1\",\"Message 2\"],\"name\":\"Leandro Costa\"}";
-		
-		User user = (User) schemaConverter.convert("json", data);
-		
-		XmlMapper xmlMapper = new XmlMapper();
-		xmlMapper.writeValue(new File("simple_bean.xml"), user);
-		File file = new File("simple_bean.xml");
-		assertNotNull(file);
-	}
+    @Test
+    public void testConverter() throws JsonGenerationException, JsonMappingException, IOException {
+        GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
+        String data = "{\"age\":31,\"messages\":[\"Message 1\",\"Message 2\",\"Message 3\",\"Message 4\"],\"name\":\"Leandro Costa\"}";
+
+        User user = (User) schemaConverter.convert("json", data);
+        Assert.assertEquals(user.getAge(), 31);
+        Assert.assertEquals(user.getName(), "Leandro Costa");
+        Assert.assertEquals(user.getMessages().size(), 4);
+    }
+
+    @Test
+    public void testConverterXML() throws JsonGenerationException, JsonMappingException, IOException {
+        GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
+        String data = "<User xmlns=\"\"><name>Leandro Costa</name><age>31</age><messages><messages>Message 1</messages><messages>Message 2</messages><messages>Message 3</messages><messages>Message 4</messages></messages></User>";
+
+        User user = (User) schemaConverter.convert("xml", data);
+        Assert.assertEquals(user.getAge(), 31);
+        Assert.assertEquals(user.getName(), "Leandro Costa");
+        Assert.assertEquals(user.getMessages().size(), 4);
+    }
+
+    @Test
+    public void testWrite() throws JsonGenerationException, JsonMappingException, IOException {
+        GenericSchemaConverter<User> schemaConverter = new GenericSchemaConverter<User>(User.class);
+        String data = "{\"age\":31,\"messages\":[\"Message 1\",\"Message 2\"],\"name\":\"Leandro Costa\"}";
+
+        User user = (User) schemaConverter.convert("json", data);
+
+        XmlMapper xmlMapper = new XmlMapper();
+        xmlMapper.writeValue(new File("simple_bean.xml"), user);
+        File file = new File("simple_bean.xml");
+        assertNotNull(file);
+    }
 
 }
